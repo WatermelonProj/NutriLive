1168648e264cf45d944396ee752360ebf5144b8d
lucasszmt <lucasszmt@gmail.com>
2017-08-10 13:25:13 -0300

    Shortcuts na home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,7 +33,7 @@
                 </div>
             </div>
         </a>
-        <a href="{{ route('cardapio.all') }}">
+        <a href="{{ route('cardapio') }}">
             <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
                 <div class="tile-stats">
                     <div class="icon"><i class="fa fa-calendar-check-o"></i></div>
